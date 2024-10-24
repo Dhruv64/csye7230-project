4691adf82de999be82db8430b63819fc5ea7f4c9
GeorgeKuncheria <chempumthara.g@northeastern.edu>
2024-10-23 21:12:58 -0400

    Added navbar and Hero sectio resolves #9

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,23 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <section className="bg-white">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt=""
+            src="/abstractBackground.png"
+            className="absolute inset-0 h-full w-full object-cover opacity-80"
+          />
+        </section>
+
+        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+          <div className="max-w-xl lg:max-w-3xl">
+            <SignUp />
+          </div>
+        </main>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from 'next/image';
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+import { Typewriter } from 'react-simple-typewriter'
+
+type Props = {}
+
+const Hero = (props: Props) => {
+  return (
+    <section className='bg-gray-100 flex-col overflow-hidden'>
+        <div className="flex flex-col overflow-hidden">
+            <ContainerScroll
+                titleComponent={
+                <>
+                    <h1 className="text-4xl font-semibold text-black dark:text-white">
+                    Transform Your Financial Journey with an AI-Driven 
+                    <br />
+                    <span className="text-4xl md:text-[6rem] text-green-500 font-bold mt-1 leading-none">
+                    <Typewriter
+                        words={['FINANCIAL ADVISOR']}
+                        loop={1}
+                        cursor
+                        cursorStyle='|'
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                        />
+                    </span>
+                    </h1>
+                </>
+                }
+            >
+                <Image
+                src='/dummyImage.png'
+                alt="hero"
+                height={720}
+                width={1400}
+                className="mx-auto rounded-2xl object-cover h-full object-left-top"
+                draggable={false}
+                />
+            </ContainerScroll>
+        </div>
+
+    </section>
+  )
+}
+
+export default Hero
