aa7db099cc0c4301daada47728528be9a77d1df0
GeorgeKuncheria <chempumthara.g@northeastern.edu>
2024-11-25 20:24:01 -0500

    Types param modified

--- types/index.d.ts
+++ types/index.d.ts
@@ -18,10 +18,12 @@ declare type CreateUserParams = {
 }
 
 declare type BudgetParams = {
+    _id?: string;
     name: string;
     amount: number;
     icon: string;
     createdBy: string;
+    createdAt?: Date;
 }
 
 declare type IncomeParams = {
@@ -31,3 +33,11 @@ declare type IncomeParams = {
     createdBy : string;
 }
 
+
+declare type ExpenseParams = {
+    name : string;
+    amount : number;
+    budgetId : string;
+    createdBy : string;
+    createdAt ?: string;
+}
